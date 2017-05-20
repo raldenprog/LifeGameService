352ef42c18d7311801a157280e76913fc1a73e61
raldenprog <raldenprog@mail.ru>
2017-05-20 16:19:22 +0700

    Paths in tests for Linux and other OS

--- Test/cabinet.py
+++ Test/cabinet.py
@@ -1,5 +1,16 @@
+#for other OS
 from app.api.user_cabinet.cabinet import user_cabinet
 
+#for Linux
+'''
+import sys
+import os
+directory_user_cabinet= os.getcwd()
+directory_user_cabinet=directory_user_cabinet.split("Test")[0]
+directory_user_cabinet+="app/api/user_cabinet"
+sys.path.insert(0, directory_user_cabinet)
+from cabinet import user_cabinet
+'''
 
 def cabinet_done():
     data = {
--- Test/registration.py
+++ Test/registration.py
@@ -1,5 +1,16 @@
+#for other OS
 from app.api.database.registration_users import add_user
 
+#for Linux
+'''
+import sys
+import os
+directory_user_cabinet= os.getcwd()
+directory_user_cabinet=directory_user_cabinet.split("Test")[0]
+directory_user_cabinet+="app/api/database"
+sys.path.insert(0, directory_user_cabinet)
+from registration_users import add_user
+'''
 
 def registration_done():
     data = {
