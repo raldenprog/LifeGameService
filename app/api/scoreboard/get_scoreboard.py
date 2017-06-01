7874b14b63003acb5a7246b136a86b7a1a93df8e
den-isk1995 <den-isk1995@mail.ru>
2017-06-01 18:52:16 +0700

    [SCOREBOARD]: Added functions for getting all users for scoreboard and for getting users by event field (without scroll). NOTABENE: We need to add some fields to users DB (events, scores)

---
+++ app/api/scoreboard/get_scoreboard.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+import logging
+import time
+from app.api.database.connect_db import db_connect
+
+logging.basicConfig(filename='logger.log',
+                    format='%(filename)-12s[LINE:%(lineno)d] %(levelname)-8s %(message)s %(asctime)s',
+                    level=logging.INFO)
+
+def all_users(count):
+    connect, current_connect = db_connect()
+    # TODO: Добавить в базу users количество очков и добавить это поле в SELECT
+    sql = "SELECT name, id FROM users LIMIT 10 OFFSET {}".format(count)
+    try:
+        current_connect.execute(sql)
+        result = current_connect.fetchall()
+    except:
+        logging.error('Fatal error: execute database')
+        return {'Answer': 'Error'}
+    return {'Answer': 'Success', 'data': result}
+
+def event_users(count, event):
+    connect, current_connect = db_connect()
+    # TODO: Добавить в базу users количество очков и добавить это поле в SELECT
+    # TODO: Добавить условие WHERE для фильтрации по event'ам
+    sql = "SELECT name, id FROM users WHERE {} LIMIT 10 OFFSET {}".format(event, count)
+    try:
+        current_connect.execute(sql)
+        result = current_connect.fetchall()
+    except:
+        logging.error('Fatal error: execute database')
+        return {'Answer': 'Error'}
+    return {'Answer': 'Success', 'data': result}
