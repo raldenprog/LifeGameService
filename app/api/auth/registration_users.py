1de7ed5a012f17a3a6ff8d8e36894bb1b6422baf
Anton Chusovitin <antonion2011@yandex.ru>
2017-10-03 22:58:48 +0700

    Обновлена регистрация и обновленна структура таблиц 1) Auth - таблица для хранения логина и пароля 2) Access - таблица прав хранит уровень доступа 3) Users - таблица для хранения данных о пользователе 4) Session - хранит UUID сессии пользователя (Для поддержания сессии на фронте) 5) Регистрация идет водопадом Auth -> Access -> Users -> Session. Сделано так для того, чтобы в случае ошибки во время регистрации, пользователь без вреда себе смог продолжить работу. Сессия в таком случае выдается только через авторизацию. Если регистрация прошла успешно, то на фронт приходит UUID сессии и в дальнейшем через него поддерживается связь.

--- app/api/auth/registration_users.py
+++ app/api/auth/registration_users.py
@@ -1,57 +1,101 @@
 import hashlib
 import logging
+import uuid
 from app.api.database.connect_db import db_connect
 logging.basicConfig(filename='logger.log',
                     format='%(filename)-12s[LINE:%(lineno)d] %(levelname)-8s %(message)s %(asctime)s',
                     level=logging.INFO)
 
 
-def add_user(user_data):
+def registration_user(user_data):
     check = ['Login', 'Password', 'Name',
-             'Patronymic', 'Email', 'Sex',
+             'Surname', 'Email', 'Sex',
              'City', 'Educational', 'Logo_name', 'Logo']
-    registration_data = {'Login': '', 'Password': '', 'Name': '',
-        'Patronymic': '', 'Email': '', 'Sex': '',
-        'City': '', 'Educational': '', 'Logo_name': '', 'Logo': ''}
-    flag = False
+    registration_data = dict.fromkeys(check, '')
+    error = False
     for data in check:
-        try:
-            if user_data[data] is None:
-                logging.info('Incorrect parameter ' + data)
-                registration_data[data] = 'Error'
-                flag = True
-            else:
-                registration_data[data] = user_data[data]
-        except:
-            logging.error('Fatal error: param ' + data)
-            registration_data[data] = 'Error'
-            flag = True
-    if flag:
+        if user_data.get(data, None) is None:
+            logging.info('Incorrect parameter ' + data)
+            registration_data[data] = 'Пустой параметр!'
+            error = True
+        else:
+            registration_data[data] = user_data[data]
+    if error:
         return {"Answer": "Error", 'Data': registration_data}
-    with open('../app/resources/logo_users/{}'.format(user_data['Logo_name']), 'w') as logo_file:
+    # TODO: Загрузить логотип в файл
+    """
+    with open('../app/resources/logo_users/{}'.format(user_data.get('Logo_name')), 'wb') as logo_file:
         logo_file.write(user_data['Logo'])
         registration_data['Logo'] = 'True'
-    return input_user_table(registration_data)
+    """
+    Answer = input_auth_table(registration_data)
+    if Answer.get('Answer') is not "Success":
+        return {'Answer': 'Warning', "Data": "Ошибка запроса к базе данных"}
+    return Answer
 
 
-def input_user_table(user_data):
+def input_auth_table(user_data):
     connect, current_connect = db_connect()
     if connect == -1:
-        return {"Answer": "Error"}
+        return {"Answer": "Warning", "Data": "Ошибка доступа к базе данных, повторить позже"}
     password_hash = hashlib.md5()
     password_hash.update(user_data['Password'].encode())
     user_data['Password'] = password_hash.hexdigest()
+    sql = "INSERT INTO Auth" \
+        " VALUES (null,\"{Login}\",\"{Password}\")".format(
+            Login=user_data.get('Login'),
+            Password=user_data.get('Password'))
+    print(sql)
     try:
-        sql = "INSERT INTO users" \
-            " VALUES (null,\"{Login}\",\"{Password}\",\"{Name}\"," \
-            "\"{Patronymic}\",\"{Email}\",\"{Sex}\",\"{City}\"," \
-            "\"{Educational}\",\"{Logo}\", 1, 1, 1)".format(**user_data)
-        print(sql)
+        current_connect.execute(sql)
+        connect.commit()
+        current_connect.execute("select last_insert_id()")
+        id_user = current_connect.fetchone()
+    except:
+        logging.error('error: Ошибка запроса к базе данных')
+        return {'Answer': 'Warning', "Data": "Ошибка запроса к базе данных"}
+    return input_access_table(id_user.get('last_insert_id()'), user_data, connect, current_connect)
+
+
+def input_access_table(id_user, user_data, connect, current_connect):
+    sql = "INSERT INTO Access" \
+        " VALUES ({id},0)".format(id=id_user)
+    print(sql)
+    try:
+        current_connect.execute(sql)
+        connect.commit()
+    except:
+        logging.error('error: Ошибка запроса к базе данных')
+        return {'Answer': 'Warning', "Data": "Ошибка запроса к базе данных"}
+    return input_user_table(id_user, user_data, connect, current_connect)
+
+
+def input_user_table(id_user, user_data, connect, current_connect):
+
+    sql = "INSERT INTO Users" \
+        " VALUES ({id_user},\"{Name}\",\"{Surname}\",\"{Email}\"," \
+        "\"{Sex}\",\"{City}\",\"{Educational}\",\"{Logo}\"" \
+        ")".format(**user_data, id_user=id_user)
+    print(sql)
+    try:
+        current_connect.execute(sql)
+        connect.commit()
+    except:
+        logging.error('error: Ошибка запроса к базе данных')
+        return {'Answer': 'Warning', "Data": "Ошибка запроса к базе данных"}
+    return input_session_table(id_user, user_data, connect, current_connect)
+
+
+def input_session_table(id_user, user_data, connect, current_connect):
+    UUID = uuid.uuid4()
+    sql = "INSERT INTO Session" \
+        " VALUES (null, {id}, \"{UUID}\")".format(id=id_user, UUID=uuid.uuid4())
+    print(sql)
+    try:
         current_connect.execute(sql)
         connect.commit()
         connect.close()
     except:
-        logging.error('Fatal error: execute database')
-        return {'Answer': 'Error'}
-
-    return {'Answer': 'Success'}
+        logging.error('error: Ошибка запроса к базе данных')
+        return {'Answer': 'Warning', "Data": "Ошибка запроса к базе данных"}
+    return {'Answer': 'Success', 'Data': {"UUID": UUID} }
