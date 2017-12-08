71a1a600db90a695479e6f64a9b2da87dbf81304
Glen Hassle <gster@gster.gster>
2017-12-08 22:58:38 +0700

    Testing route to show_event::all_event with different parametrs

---
+++ Test/route_all_event.py
@@ -0,0 +1,20 @@
+import json
+import pprint
+import requests
+
+def all_event_OK():
+    dataOK = {'id_event': 0}
+    r = requests.get('http://0.0.0.0:13451/allevents', data=dataOK)
+    pprint.pprint(json.loads(r.text))
+
+def all_event_NO():
+    dataNO1 = {'user': "Anton"}
+    r = requests.get('http://0.0.0.0:13451/allevents', data=dataNO1)
+    pprint.pprint(json.loads(r.text))
+    dataNO2 = {'login':'test'}
+    r = requests.get('http://0.0.0.0:13451/allevents', data=dataNO2)
+    pprint.pprint(json.loads(r.text))
+
+all_event_OK()
+print()
+all_event_NO()
