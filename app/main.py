8cf3ed7037dc5cc7a1582a2a109f01fbe6210cc3
Glen Hassle <gster@gster.gster>
2017-12-14 00:23:06 +0700

    изменил название роута и класса на Event, то же в main

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from route.Authentication import Authentication
 from route.Task import Task
 from route.Logout import Logout
 from route.Scoreboard import Scoreboard
-from route.show_all_event import Show_all_event
+from route.Event import Event
 
 _app = Flask(__name__)
 _app.config['JSON_AS_ASCII'] = False
@@ -38,7 +38,7 @@ api.add_resource(Task, '/task')
 api.add_resource(Scoreboard, '/scoreboard')
 api.add_resource(Logout, '/logout')
 api.add_resource(index, '/')
-api.add_resource(Show_all_event, '/event')
+api.add_resource(Event, '/event')
 
 
 if __name__ == '__main__':
---
+++ app/route/Event.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+from flask_restful import Resource, reqparse
+from flask import request
+import api.event.show_event as show  # show_event
+from api.Converter_json import converter
+
+class Event(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('page')
+        args = parser.parse_args()
+        print('GET /')
+        print(request.headers)
+        print('cookies = ', request.cookies)
+        print('ARGS = ', args)
+        page = args.get('page', None)
+        try:
+            answer = converter(show.all_event(int(page)))
+        except:
+            return {'Answer':"Error"}, 200, {'Access-Control-Allow-Origin': '*'}
+        return answer, 200, {'Access-Control-Allow-Origin': '*'}
