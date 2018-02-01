101a251b5e01a41db1bd181c139be75f11901577
raldenprog <raldenprog@mail.ru>
2018-02-01 18:06:23 +0700

    поправки к logo

--- app/api/logo/logo.py
+++ app/api/logo/logo.py
@@ -4,7 +4,5 @@ def image_to_base64(image_name):
     image = open(image_name, 'rb')
     image_read = image.read()
     image_64_encode = base64.encodestring(image_read)
-    return (image_64_encode)
+    return ("data:image+xml;base64," + image_64_encode.decode("utf-8"))
     #далее вставляем в html <img src="data:image+xml;base64,[base64]">, что позволит выводить фаотографии png,jpg, если надо svg, то <img src="data:image/svg+xml;base64,[base64]">
-    
-    
