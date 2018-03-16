5806e6139befcf2d8e05db3f578b77086876cd4e
raldenprog <raldenprog@mail.ru>
2018-03-16 22:01:17 +0700

    добавил функцию converter переписал автоизацию на get запрос

--- app/api/service.py
+++ app/api/service.py
@@ -60,3 +60,11 @@ class GameService:
         except:
             logging.error('Fatal error: check id')
             return {names.ANSWER: names.ERROR}
+    @staticmethod
+    def converter(js):
+        """
+        Метод преобразовывает передаваемый json в Dict и наоборот
+        :param js: str или json
+        :return: str или dict преобразованный элемент
+        """
+        return json.dumps(js) if isinstance(js, dict) else json.loads(js)
--- app/route/Authentication.py
+++ app/route/Authentication.py
@@ -1,34 +1,40 @@
 # coding=utf-8
 import json
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import request
 from api.config import HEADER
 import api.auth.login_user as auth
 import api.base_name as names
+from api.service import GameService as gs
 
 
 class Authentication(Resource):
+    def __init__(self):
+        self.__parser = reqparse.RequestParser()
+        self.__parser.add_argument('data')
+        self.__args = self.__parser.parse_args()
+        self.data = None
+
+    def parse_data(self):
+        self.data = self.__args.get('data', None)
+        self.data = gs.converter(self.data)
+        return
+
+    def check_data(self):
+        if self.data[names.LOGIN] is None:
+            return False
+        if self.data[names.PASSWORD] is None:
+            return False
+        return True
+
+    def switch(self):
+        answer = auth.login_verification(self.data)
+        return answer
+
     def get(self):
-        print('GET /')
-        print(request.headers)
-        print('cookies = ', request.cookies)
-        print('ARGS = ', request.form)
-        return {'test': 'test'}, 200, HEADER
-
-    def post(self):
-        print('POST /auth')
-        print(request.headers)
-        print('cookies = ', request.cookies)
-        print('ARGS = ', request.form)
-        url = json.loads(request.data.decode())[names.DATA]
-        print(url)
-        answer = auth.login_verification(url)
-        print(answer)
-        return answer, 200, {'Access-Control-Allow-Origin': '*'}
-
-    def options(self):
-        return {'Allow': 'POST'}, 200, {'Access-Control-Allow-Origin': '*',
-                                        'Access-Control-Allow-Methods': 'POST,GET',
-                                        'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin, '
-                                                                        'Content-Type, '
-                                                                        'X-Custom-Header'}
+        self.parse_data()
+        check = self.check_data()
+        if check:
+            answer = self.switch()
+            return answer, 200, {'Access-Control-Allow-Origin': '*'}
+        return "Error",  200, {'Access-Control-Allow-Origin': '*'}
