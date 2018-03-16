a3e201e0cbd5ef366dc535f1daa9c3dde7bb00f5
raldenprog <raldenprog@mail.ru>
2018-03-16 23:02:53 +0700

    route cabinet

--- app/route/Cabinet.py
+++ app/route/Cabinet.py
@@ -4,25 +4,34 @@ from flask import request
 from api.user_cabinet.cabinet import edit_cabinet
 from api.user_cabinet.cabinet import user_cabinet
 import api.base_name as names
-
+from api.service import GameService as gs
 
 class Cabinet(Resource):
+    def __init__(self):
+        self.__parser = reqparse.RequestParser()
+        self.__parser.add_argument('data')
+        self.__parser.add_argument('param')
+        self.__args = self.__parser.parse_args()
+        self.data = None
+        self.param = None
+
+    def parse_data(self):
+        self.data = self.__args.get('data', None)
+        self.param = self.__args.get('param', None)
+        print(self.param)
+        print(self.data)
+        self.data = gs.converter(self.data)
+        return
+    def switch(self):
+        print(self.param)
+        if self.param == "edit" and self.data is not None:
+            answer = edit_cabinet(self.data)
+        else:
+            answer = user_cabinet(self.data)
+        return answer
+
     def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument(names.ID)
-        args = parser.parse_args()
-        print('GET /cabinet')
-        data = args.get(names.DATA, None)
-        id_user = args.get(names.ID, None)
-        print(request.headers)
-        print('cookies = ', request.cookies)
-        print('ARGS = ', request.form)
-        param = args.get('param', None)
-        try:
-            if param == "edit" and data is not None:
-                answer = edit_cabinet({names.DATA: data})
-            else:
-                answer = user_cabinet({names.ID_USER: id_user})
-        except:
-            return {names.ANSWER: names.ERROR}, 200, {'Access-Control-Allow-Origin': '*'}
+        self.parse_data()
+        answer = self.switch()
         return answer, 200, {'Access-Control-Allow-Origin': '*'}
+#        return "Error", 200, {'Access-Control-Allow-Origin': '*'}
