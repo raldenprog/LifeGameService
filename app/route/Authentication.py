955beb5bc0413487f5a607ff9df9084143a92628
RaldenProG <raldenprog@mail.ru>
2018-03-25 21:43:48 +0700

    more route

--- app/route/Authentication.py
+++ app/route/Authentication.py
@@ -4,41 +4,43 @@ from flask_restful import Resource, reqparse
 import api.auth.login_user as auth
 import api.base_name as names
 from api.service import GameService as gs
+from api.auth.auth import session_verification
 
 
 class Authentication(Resource):
     def __init__(self):
         self.__parser = reqparse.RequestParser()
         self.__parser.add_argument('data')
+        self.__parser.add_argument('param')
         self.__args = self.__parser.parse_args()
         self.data = None
 
     def parse_data(self):
         self.data = self.__args.get('data', None)
+        self.param = self.__args.get('param', None)
+        print("param:", self.param)
         self.data = gs.converter(self.data)
         print("data: ", self.data)
         return
 
-    def check_data(self):
-        if self.data[names.LOGIN] is None:
-            return False
-        if self.data[names.PASSWORD] is None:
-            return False
-        return True
-
     def switch(self):
-        answer = auth.login_verification(self.data)
-        return answer
+        if self.param == "get_user_name" and self.data is not None:
+            self.data["id_user"] = session_verification(self.data["UUID"])
+            print(self.data["id_user"])
+            answer = gs.converter(auth.get_user_name(self.data["id_user"]))
+            return answer
+        else:
+            answer = auth.login_verification(self.data)
+            return answer
 
     def get(self):
         try:
             print("Auth")
             self.parse_data()
-            check = self.check_data()
-            if check:
-                answer = self.switch()
-                print("answer: ", answer)
-                return answer, 200, {'Access-Control-Allow-Origin': '*'}
-            return "Error",  200, {'Access-Control-Allow-Origin': '*'}
+
+            answer = self.switch()
+            print("answer: ", answer)
+            return answer, 200, {'Access-Control-Allow-Origin': '*'}
+
         except:
             return "Error", 200, {'Access-Control-Allow-Origin': '*'}
