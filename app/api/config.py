d24dfb266069c91f4a31eda6c7b60ab5cbaa465a
Андрей Крылосов <ssttoorrmma@gmail.com>
2019-03-05 23:57:07 +0700

    фикс адреса бд

--- app/api/config.py
+++ app/api/config.py
@@ -1,7 +1,7 @@
 HEADER = {'Access-Control-Allow-Origin': '*'}
 DATABASE = {
-    "dbname": "inside",
-    "user": "inside_user",
+    "dbname": "life_game_service",
+    "user": "dev_life_user",
     "host": "192.168.1.3",
-    "password": "PINLOX!@#PINLOX!@#"
+    "password": "PINLOX!@#"
 }
